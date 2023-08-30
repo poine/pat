1af79ce207b19b16f8bdd571722fd54c0aebf355
poine <poine@manu.localdomain>
2023-08-30 08:57:12 +0200

    reference model

--- src/pat3/test/test_reference.py
+++ src/pat3/test/test_reference.py
@@ -1,50 +1,71 @@
 #!/usr/bin/env python3
 import sys, time, numpy as np
 import scipy.signal
-#import rospy
+
 import matplotlib.pyplot as plt
 
 import pat3.utils as p3_u
 import pat3.plot_utils as p3_pu
 
+'''
+Unit test for saturated linear reference model
+'''
+
+import pdb
+
 def plot_ref(_time, Xr, _id="", _sp=None, fig=None, axs=None):
-    fig, axs = plt.subplots(3, 1) if fig is None else (fig, axs)
-    axs[0].plot(_time, Xr[:,0], label=_id)
+    #pdb.set_trace()
+    fig, axs = plt.subplots(Xr.shape[1], 1) if fig is None else (fig, axs)
+    for i in range(Xr.shape[1]):
+        axs[i].plot(_time, Xr[:,i], label=_id)
+        p3_pu.decorate(axs[i], f'$x^{{({i})}}$')
     if _sp is not None: axs[0].plot(_time, _sp, label='setpoint')
-    p3_pu.decorate(axs[0], 'pos', legend=True)
-    axs[1].plot(_time, Xr[:,1])
-    p3_pu.decorate(axs[1], 'vel')
-    axs[2].plot(_time, Xr[:,2])
-    p3_pu.decorate(axs[2], 'accel')
+    p3_pu.decorate(axs[0], 'x', legend=True)
     return fig, axs
 
 def run_ref(_ref, _time, _sp):
-    Xr = np.zeros((len(_time), 3))
+    Xr = np.zeros((len(_time), _ref.order+1))
     for i in range(1, len(_time)):
-        dt = _time[i] - _time[i-1]
-        Xr[i] = _ref.run(dt, _sp[i])
+        Xr[i] = _ref.run( _time[i] - _time[i-1], _sp[i])
     return Xr
+
+def _sec_order():
+    poles, sats = p3_u.omxi_to_lambda(6., 0.9), [5., 25.]  # vel, accel
+    return poles, sats
+
+def _third_order():
+    poles, sats = [-4, -4+2j, -4-2j], [6., 25., 50.] # vel accel jerk
+    return poles, sats
+
+def _fourth_order():
+    poles = p3_u.omxi_to_lambda(3, 0.9)+p3_u.omxi_to_lambda(6, 0.8)
+    sats = [6., 25., 50., 500.] # vel accel jerk jerkd
+    return poles, sats
+
+def _fifth_order():
+    poles = p3_u.omxi_to_lambda(3, 0.9)+p3_u.omxi_to_lambda(6, 0.8)+ (-10.,)
+    sats = [6., 25., 50., 500., 2500.] # vel accel jerk jerkd
+    return poles, sats
     
-def test_scalar_ref():
+def test(_ref_params, show_lin=False):
+    poles, sats = _ref_params()
+    K = -np.real(np.polynomial.polynomial.polyfromroots(poles)[:-1])
+    _ref1 = p3_u.LinRef(K, sats)
     _time = np.arange(0, 10, 0.01)
-
-    #_sats = [6., 50.]  # vel, accel
-    _sats = [6., 25.]  # vel, accel
-    _ref1 = p3_u.SecOrdLinRef(omega=6, xi=0.9, sats=None)
-    _ref2 = p3_u.SecOrdLinRef(omega=6, xi=0.9, sats=_sats)
-
-    #_sp = 1.*np.ones(len(_time))
     _sp = 4.*scipy.signal.square(_time*np.pi/3)
     Xr1 = run_ref(_ref1, _time, _sp)
-    Xr2 = run_ref(_ref2, _time, _sp)
-
-    fig, axs = plot_ref(_time, Xr1, _id="linear")
-    plot_ref(_time, Xr2, _id="saturated", _sp=_sp, fig=fig, axs=axs)
+    fig, axs = plot_ref(_time, Xr1, _id="saturated")
+    if show_lin:
+        _ref2 = p3_u.LinRef(K, None)
+        Xr2 = run_ref(_ref2, _time, _sp)
+        plot_ref(_time, Xr2, _id="linear", _sp=_sp, fig=fig, axs=axs)
     plt.show()
-        
-def main(args):
-    test_scalar_ref()
     
+def main(args):
+    test(_sec_order, True)
+    #test(_third_order)
+    #test(_fourth_order)
+    #test(_fifth_order)
       
 if __name__ == '__main__':
     main(sys.argv)
