fixed quaternion component ordering change for new ros2 tf code

ros2_pat/ros2_pat/traj_publisher.py
@@ -1,7 +1,7 @@
 ##
 #
 # python3  ros2_pat/ros2_pat/traj_publisher.py --ros-args -p traj_name:=si2 -p list:=true
-
+#
 
 
 import rclpy
@@ -25,8 +25,6 @@ import pat3.algebra as pal
 
 import ros2_pat.utils
 
-FOO = True
-
 class TrajPublisher(Node):
 
     def __init__(self):
@@ -41,35 +39,17 @@ class TrajPublisher(Node):
         
         traj_name = self.get_parameter('traj_name').get_parameter_value().string_value
         self.get_logger().info(f"loading trajectory: {traj_name}")
-        #self.traj = trjf.Traj43(duration=7.)
         self.traj, desc = trjf.get(traj_name)
         self.get_logger().info(f'  Description: {desc}')
         
         self.tf_pub = ros2_pat.utils.TransformPublisher(self)
         self.tf_pub.send_w_enu_to_ned_transform(rclpy.clock.Clock().now())
 
-        
-
-        if FOO:
-            self.marker_pub = self.create_publisher(Marker, "/pat/reference", 2)
-            self.marker = Marker()
-            self.marker.header.frame_id = "/w_ned"
-            self.marker.type = 2
-            self.marker.id = 0
-            self.marker.scale.x = 1.0
-            self.marker.scale.y = 1.0
-            self.marker.scale.z = 1.0
-            self.marker.color.r = 0.0
-            self.marker.color.g = 1.0
-            self.marker.color.b = 0.0
-            self.marker.color.a = 1.0
-            self.marker.type = 10 #visualization_msgs::Marker::MESH_RESOURCE;
-            #self.marker.mesh_resource = "package://ros2_pat/meshes/quadcopter.dae"
-            self.marker.mesh_resource = "package://ros2_pat/meshes/quad.dae"
-            #self.marker.mesh_resource = "package://ros2_pat/meshes/mavic.dae"
-        else:
-            self.marker_pub = ros2_pat.utils.MarkerPublisher(self, '/pat/reference', mtype=10,
-                                                             mesh='package://ros2_pat/meshes/quad.dae')
+        self.vehicle_marker_pub = ros2_pat.utils.MarkerPublisher(self, '/pat/reference', mtype=10,
+                                                                 mesh='package://ros2_pat/meshes/quad.dae',
+                                                                 #mesh="package://ros2_pat/meshes/quadcopter.dae",
+                                                                 #mesh="package://ros2_pat/meshes/mavic.dae",
+                                                                 color=[0., 1., 0., 1.], frame_id="/w_ned")
 
 
         try:
@@ -79,12 +59,10 @@ class TrajPublisher(Node):
                                                               colors=[[1., 0.2, 0.2, 0.5]]*nwp, scales=[(0.1, 0.1, 0.1)]*nwp)
         except AttributeError: pass
             
-        self.trj_pub = ros2_pat.utils.TrajPublisher(self.traj, self, 'pat/ref_trajectory')
-
+        self.trj_pub = ros2_pat.utils.TrajPublisher(self.traj, self, 'pat/ref_trajectory', color=[0.5, 0.75, 0.75, 0.25])
             
         timer_period = 1./20.  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-
         
         self.df = ctl.DiffFlatness()
         self.fdm = fdm.FDM()
@@ -97,21 +75,15 @@ class TrajPublisher(Node):
         t = np.fmod(now.nanoseconds*1e-9 - self.t0, self.traj.duration)
         Y = self.traj.get(t)
         X, U, Xd = self.df.state_and_cmd_of_flat_output(Y, self.fdm.P)
-        if FOO:
-            self.marker.header.stamp = now.to_msg()
-            self.marker.pose.position.x = X[fdm.sv_x] #np.sin(self.i%100/50.*np.pi) #0.
-            self.marker.pose.position.y = X[fdm.sv_y]
-            self.marker.pose.position.z = X[fdm.sv_z]
-            self.marker.pose.orientation.x = X[fdm.sv_qx]
-            self.marker.pose.orientation.y = X[fdm.sv_qy]
-            self.marker.pose.orientation.z = X[fdm.sv_qz]
-            self.marker.pose.orientation.w = X[fdm.sv_qi]
-            self.marker_pub.publish(self.marker)
-        else:
-            #T_ned2b = np.eye(4)
-            T_ned2b = ros2_pat.utils.T_of_t_q(X[fdm.sv_slice_pos], X[fdm.sv_slice_quat])
-            self.marker_pub.publish(now, T_ned2b)
-        
+        #T_ned2b = ros2_pat.utils.T_of_t_q(X[fdm.sv_slice_pos], X[fdm.sv_slice_quat])
+        # from fdm
+        #sv_qi   = 6  # world to body rotation quaternion
+        #sv_qx   = 7  #
+        #sv_qy   = 8  #
+        #sv_qz   = 9  #
+        qalt = [X[fdm.sv_qx],X[fdm.sv_qy],X[fdm.sv_qz],X[fdm.sv_qi]]
+        T_ned2b = ros2_pat.utils.T_of_t_q(X[fdm.sv_slice_pos], qalt)
+        self.vehicle_marker_pub.publish(now, T_ned2b)
         self.trj_pub.publish()
         try:
             nwp = len(self.traj._g.waypoints)

ros2_pat/ros2_pat/utils.py
@@ -8,8 +8,6 @@ import visualization_msgs.msg, geometry_msgs.msg
 
 import numpy as np
 
-import pdb
-
 class TransformPublisher:
     def __init__(self, parent_node):
         self.tf_broadcaster = tf2_ros.TransformBroadcaster(parent_node)
@@ -113,12 +111,18 @@ class TrajPublisher:
         self.msg.pose.orientation.w = 1.
         self.msg.scale.x, self.msg.scale.y, self.msg.scale.z = 0.01, 0.01, 0.01
         self.msg.color.r, self.msg.color.g, self.msg.color.b, self.msg.color.a = color
-        for l in np.linspace(0,1, 100):
-        #for _p in [[0.,0.,0.], [0.,1.,0.], [2.,1.,0.]]:
-            #pdb.set_trace()
-            p = geometry_msgs.msg.Point()
-            p.x, p.y, p.z = traj._g.get(l)[:3,0]
-            self.msg.points.append(p)
+        if 0:
+            for l in np.linspace(0,1, 100):
+                #for _p in [[0.,0.,0.], [0.,1.,0.], [2.,1.,0.]]:
+                #pdb.set_trace()
+                p = geometry_msgs.msg.Point()
+                p.x, p.y, p.z = traj._g.get(l)[:3,0]
+                self.msg.points.append(p)
+        else:
+            for t in np.linspace(traj.t0, traj.t0+traj.duration, 600):
+                p = geometry_msgs.msg.Point()
+                p.x, p.y, p.z = traj.get(t)[:3,0]
+                self.msg.points.append(p)
         
     def publish(self):
         #print(self.msg, '\n')